11d723c6bb57f41b66979dae47a62c29ca4d518a
Grigoriy56 <grisha9hs@gmail.com>
2022-04-10 21:29:41 +0300

    now added parcer for TinkoffAPI

--- 3 websockets/tinkoff_parcer.py
+++ 3 websockets/tinkoff_parcer.py
@@ -2,13 +2,9 @@ import bs4 as bs
 import requests
 import json
 # {"bounds":{"bottomLeft":{"lat":55.81125000638905,"lng":49.0942425944959},"topRight":{"lat":55.82671338419883,"lng":49.16295006062138}},"filters":{"banks":["tcs"],"showUnavailable":true,"currencies":["USD"],"amounts":[{"currency":"USD","amount":100}]},"zoom":15}
-data1 = {"bounds":
-             {"bottomLeft":{"lat":55.811285724635724,"lng":48.98957303927949},
-              "topRight":{"lat":55.86441518775052,"lng":49.52446988986542}},
-         "filters":
-             {"banks":["tcs"],"showUnavailable":True,"currencies":["USD"],"amounts":[
-                 {"currency":"USD"}]}
-    ,"zoom":12}
+js =  {"bounds":{"bottomLeft":{"lat":55.81125000638905, "lng":49.0942425944959}, "topRight":{"lat":55.82671338419883, "lng":49.16295006062138}}, "filters":{"banks":["tcs"], "showUnavailable":True, "currencies":["USD"], "amounts":[{"currency": "USD", "amount":100}]}, "zoom":13}
+
+
 headers = {
 "Host": "api.tinkoff.ru",
 "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0",
@@ -26,5 +22,14 @@ headers = {
 "TE": "trailers"}
 url = "https://api.tinkoff.ru/geo/withdraw/clusters"
 
-res = requests.post(url, data=json.dumps(data1), headers=headers, )
-print(res.text)
+res = requests.post(url, data=json.dumps(js), headers=headers)
+
+jsn_statem = json.loads(res.text.replace("'", '"'))
+clusters = jsn_statem["payload"]["clusters"]
+ATMs = []
+for cluster in clusters:
+    points = cluster["points"]
+    for point in points:
+        ATM = [point["address"], point['limits']]
+        ATMs.append(ATM)
+# print(res.text)
